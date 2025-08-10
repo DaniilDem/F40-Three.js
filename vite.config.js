384073ccbec29122e17eb61e19e6893dcf2d9f9b
d.demchenko <d.demchenko@corp.mail.ru>
2025-08-10 20:21:12 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { copySync } from "fs-extra";
+
+function copyStatic() {
+  return {
+    name: "copy-static",
+    closeBundle() {
+      // Копируем папки sfx и models в docs
+      copySync(resolve("sfx"), resolve("docs/sfx"));
+      copySync(resolve("models"), resolve("docs/models"));
+    },
+  };
+}
 
 export default defineConfig({
   base: "/F40-Three.js/", // для gh-pages
@@ -7,4 +20,5 @@ export default defineConfig({
     emptyOutDir: true,
   },
   assetsInclude: ["**/*.gltf", "**/*.mp3", "**/*.wav"],
+  plugins: [copyStatic()],
 });
