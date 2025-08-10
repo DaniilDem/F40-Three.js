14cb6d979695beb65ad3c7a7c993db644bb14e4a
d.demchenko <d.demchenko@corp.mail.ru>
2025-08-10 20:04:02 +0200

    rename dist to docs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/F40-Three.js/", // для gh-pages
   build: {
-    outDir: "dist",
+    outDir: "docs",
     emptyOutDir: true,
   },
   assetsInclude: ["**/*.gltf", "**/*.mp3", "**/*.wav"],
